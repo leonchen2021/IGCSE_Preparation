4737d252d63e13b4c7bbea77b21e6bcd32a702fa
Leon <leon.chen.kia@gmail.com>
2022-02-14 15:42:58 +0800

    hello

--- work.py
+++ work.py
@@ -1,1 +1,8 @@
-print("Hello world")
+#Data values
+#data value =  piece of data
+#Input = data values going into the computer 
+#store = data values in memory
+#process
+#output = results
+#"" = ''
+print("What a wonderful world")
