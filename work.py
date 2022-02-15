fa016d0928c8b7cc7a6e67d9fd5d956775a4b369
Leon <leon.chen.kia@gmail.com>
2022-02-15 15:11:30 +0800

    hello

--- work.py
+++ work.py
@@ -1,8 +1,14 @@
-#Data values
-#data value =  piece of data
-#Input = data values going into the computer 
-#store = data values in memory
-#process
-#output = results
-#"" = ''
+# Data values
+# data value =  piece of data
+# Input = data values going into the computer 
+# store = data values in memory
+# process
+# output = results
+# "" = ''
 print("What a wonderful world")
+print(123.456)
+# predefinal functions =  Python already knows
+print("==================================")
+print("          W E L C O M E          ")
+print("    This is my first program     ")
+print("==================================")
