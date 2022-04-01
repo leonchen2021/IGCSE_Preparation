4be5c09bab547baf5cace86e77d0e7e526e34e08
Leon <leon.chen.kia@gmail.com>
2022-04-01 16:47:02 +0800

    hello

--- Semester_2_fundamental/battleship.py
+++ Semester_2_fundamental/battleship.py
@@ -21,9 +21,6 @@ print(zero2)
 
 #%%
 
-zero2[3][2] = 1
-
-print(zero2)
 #%%
 
 def ask_player():
@@ -36,3 +33,23 @@ def ask_player():
     print(zero2)
 
 ask_player()
+
+#%%
+
+import numpy as np
+
+def print_board(b):
+    for row in b:
+        print(" ".join(row))
+
+def make_board():
+    board=np.array([["O"]* 10]* 10)
+    for i in range(10):
+        board[10-i-1,0]=i
+        board[9, 1:10] = np.array(list("ABCDEFGHI"))
+    board[9, 0] = " "
+    return board
+
+b = make_board()
+print_board(b)
+# %%
