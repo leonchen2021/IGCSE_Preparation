c4342099f1af9c2d9b6ed99f0212d6b78fe04ca6
Leon <leon.chen.kia@gmail.com>
2022-04-08 16:17:43 +0800

    god

--- Semester_2_fundamental/battleship.py
+++ Semester_2_fundamental/battleship.py
@@ -50,3 +50,23 @@ def make_board():
 
 b = make_board()
 print_board(b)
+
+#%%
+
+def select_target(b):
+    letter_guess = ""
+    while letter_guess not in b[9,1::]:
+        letter_guess = input("Please select horizontal position: ").upper()
+        if letter_guess not in b[9,1::]:
+            print("Sorry, that selection is not valid. Please try again.")
+
+    number_guess = ""
+    while number_guess not in b[0:9,0]:
+        number_guess = input("Please select vertical position: ")
+        if number_guess not in b[0:9,0]:
+            print("Sorry, that selection is not valid. Please try again.")
+
+    letter_coor = np.where(b[9]==letter_guess)[0][0]
+    number_coor = np.where(b[0:9,0]==number_guess)[0][0]
+
+    return [number_coor,letter_coor]
