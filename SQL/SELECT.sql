8c7ce252ca3c793f19ef176b3aa9034c6e71770b
Leon <leon.chen.kia@gmail.com>
2022-04-21 16:28:55 +0800

    good

--- SQL/SELECT.sql
+++ SQL/SELECT.sql
@@ -1,3 +1,24 @@
-SELECT * student_name
-FROM computer_science_students
-WHERE student_grade = "A"   
+-- Create school database to store our data (tables)
+DROP DATABASE IF EXISTS `school`;
+CREATE DATABASE `school`;
+USE `school`;
+
+-- Create a table for students
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- add data entries (rows) in our students table
+INSERT INTO students VALUES (1, 'Henry', 'henry@gmail.com', "Male");
+INSERT INTO students VALUES (2, 'Annie', 'annie@gmail.com', "Femle");
+INSERT INTO students VALUES (3, 'Leon', 'leon@gmail.com', "Male");
+INSERT INTO students VALUES (4, 'Charlie', 'charlie@gmail.com', "Male");
+
+SELECT student_name, student_email
+FROM school
+WHERE gender = "Male"
