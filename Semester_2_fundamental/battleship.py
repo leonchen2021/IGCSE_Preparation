f08d991ec99ca4e1c6f239a2c2af7a7722bd1602
Leon <leon.chen.kia@gmail.com>
2022-05-02 15:14:40 +0800

    good

--- Semester_2_fundamental/battleship.py
+++ Semester_2_fundamental/battleship.py
@@ -34,3 +34,40 @@ def select_target(b):
     number_coor = np.where(b[0:9,0]==number_guess)[0][0]
 
     return [number_coor,letter_coor]
+
+#%%
+
+def place_ship(size):
+    orientation = np.random.choice(["Horizontal", "Vertical"])
+
+    if orientation == "Horizontal":
+        vert_pos = np.random.choice(range(9))
+        hori_pos = np.random.choice(range(1,11-size))
+        ship_coor = [[vert_pos,i] for i in range(hori_pos,hori_pos+size)]
+
+    if orientation == "Veritcal":
+        vert_pos = np.random.choice(range(0,10-size))
+        hori_pos = np.random.choice(range(1,10))
+        ship_coor = [[i, hori_pos] for i in range(vert_pos+size)]
+
+    return ship_coor
+
+def place_all_ships():
+    all_ships = [place_ship(5)]
+    all_positions = all_ships[0].copy()
+    for size in [4,3,3,2]:
+        repeat = True
+        while repeat == True:
+            val = 0
+            new_ship = place_ship(size)
+            for i in new_ship:
+                if i not in all_positions:
+                    val += 1
+            if val == size:
+                repeat = False
+
+        all_ships.append(new_ship)
+        for point in new_ship:
+            all_positions.append(point)
+
+    return all_ships, all_positions
